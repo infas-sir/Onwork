c2fc6773ff9483d04402deaa5e1c65b0985dc270
Infas.mk <97841805+infas-sir@users.noreply.github.com>
2023-09-03 08:32:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var options = {
+  strings: ["<i>First</i> sentence.", "amp; introducing about me", "Your Order Has Been Placed :2370628
+
+
+Service :
+IG » Views + Tv + Reels [ New - Fastest - Instant ] 🔥
+
+Link :
+https://www.instagram.com/reel/CwnXloyPLTH/?igshid=MzRlODBiNWFlZA==
+
+Quantity :8000
+
+"],
+  typeSpeed: 10
+}
+
+var typed = new Typed(".typed_text", options);
