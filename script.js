795606db85a4892f5e3e5dc236e48a63b46d0cf2
Infas.mk <97841805+infas-sir@users.noreply.github.com>
2023-09-03 08:35:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 var options = {
-  strings: ["<i>First</i> sentence.", "amp; introducing about me", "Your Order Has Been Placed :2370628
-
-
-Service :
-IG » Views + Tv + Reels [ New - Fastest - Instant ] 🔥
-
-Link :
-https://www.instagram.com/reel/CwnXloyPLTH/?igshid=MzRlODBiNWFlZA==
-
-Quantity :8000
-
-"],
+  strings: ["<i>First</i> sentence.", "amp; introducing about me", "Your Order Has Been Placed :2370"],
   typeSpeed: 10
 }
 
